df2604965bd001114c829ab0f6ff7eceed64dc9e
sonndy <123dykai@gmail.com>
2021-08-24 16:41:40 -0400

    added error message under input if input is empty

--- glass.js
+++ glass.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         let newTimesMinutesAm = date.format(d, "mm A");
         let sepClass = "notrans"
 
-        if(d.getSeconds()%2 === 1) sepClass = "trans"
+        if(d.getSeconds()%2 === 1) sepClass = "trans";
         var sep = `<span class= "${sepClass}">:</span>`;
 
         let newDate = date.format(d, "MMM D Y")
@@ -30,61 +30,83 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let clearButton = document.getElementById("clearButton");
     let container = document.querySelector(".container");
     let numberOfTodo = 0;
-    let toDoArray = ["Zero","One", "Two", "Three", "Four", "Five", "Six"]
-
+    // let toDoArray = ["Zero","One", "Two", "Three", "Four", "Five", "Six"]
+    let errorMessage = document.createElement("div");
+        let sectionToAppend = document.querySelector(".mainSection")
+        errorMessage.setAttribute("class", "error-message");
+            
+        errorMessage.textContent = "The input box cannot be empty";
+        sectionToAppend.append(errorMessage);
 
     const getEnterItem = function(e) 
     {
         e.preventDefault();
         let userInput = document.getElementById("userInput").value;
-        let list = document.createElement("div");
-        list.classList.add("card");
-        numberOfTodo++;
-        list.innerHTML = 
-        ` <div class="content">
-            <h2>0${numberOfTodo}</h2>
-            <h3>TO DO</h3>
-                <p>${userInput}</p>
-        </div>
-        `;
-
-        let listButton = document.createElement("button");
-        listButton.setAttribute("id", "delete-button")
-        listButton.innerHTML = `click to remove`;
-
-
-        list.append(listButton);
-         
-
-        container.append(list);
-        document.getElementById("userInput").value = "";
-       
-
-        let newCards = document.querySelectorAll(".card");
-        newCards.forEach(card => { 
-                card.addEventListener("click", () => {
-                    if(card.classList[0] === "card")
-                    {
         
-                           card.remove();
-                            numberOfTodo--;
-                            if(numberOfTodo < 0) {
-                                numberOfTodo = 0;
-                            }
-                        
-                    }
-                })
+        
+        if(userInput === "")
+        {
+            errorMessage.classList.add("shown");
+
+        } else {
+            if(errorMessage.classList.contains("shown"))
+            {
+                console.log(errorMessage)
+                errorMessage.classList.toggle("shown")
+            }
             
-        });
+            let list = document.createElement("div");
+            list.classList.add("card");
+            numberOfTodo++;
+            list.innerHTML = 
+            ` <div class="content">
+                <h2>0${numberOfTodo}</h2>
+                <h3>TO DO</h3>
+                    <p>${userInput}</p>
+            </div>
+            `;
+
+            let listButton = document.createElement("button");
+            listButton.setAttribute("id", "delete-button")
+            listButton.innerHTML = `click to remove`;
+
+
+            list.append(listButton);
             
 
-          clearButton.addEventListener("click", ()=> {
-              list.remove();
-              numberOfTodo = 0;
-          }) 
+            container.append(list);
+            document.getElementById("userInput").value = "";
+        
+
+            let newCards = document.querySelectorAll(".card");
+            newCards.forEach(card => { 
+                    card.addEventListener("click", () => {
+                        if(card.classList[0] === "card")
+                        {
+            
+                            card.remove();
+                                numberOfTodo--;
+                                if(numberOfTodo < 0) {
+                                    numberOfTodo = 0;
+                                }
+                            
+                        }
+                    })
+                
+            });
+                
+
+            clearButton.addEventListener("click", ()=> {
+                list.remove();
+                numberOfTodo = 0;
+                errorMessage.textContent = "";
+
+            }) 
 
     }
 
+        }
+        
 
 
 
